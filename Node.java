c233497b99583358fcb915776f1f74b1df51fc25
vitin2212 <137333989+vitin2212@users.noreply.github.com>
2025-05-05 21:50:16 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,7 +1,9 @@
+// Classe Node representa um nó da árvore binária usada para o código Morse
 public class Node {
     public char letter;
     public Node left, right;
 
+     // Construtor: inicializa o nó com uma letra (ou espaço ' ' se for intermediário)
     public Node(char letter) {
         this.letter = letter;
     }
